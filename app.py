22e951871b3758826ac6054579e844a12dded1d2
jimshih <jim732883@gmail.com>
2024-11-20 15:52:44 +0800

    feat: modify connection method to use client rather than session

--- app.py
+++ app.py
@@ -14,13 +14,15 @@ CLIENT_ID = os.getenv("USER_POOL_CLIENT_ID")
 REGION_NAME = os.getenv("USER_POOL_REGION_NAME")
 
 # Initialize Cognito Identity Provider Client
-session = boto3.Session(
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
-)
-client = session.client("cognito-idp", region_name=REGION_NAME)
+# if you want to use session to connect to cognito instead of client then use below code
+# session = boto3.Session(
+#     aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+#     aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+#     aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
+# )
+# client = session.client("cognito-idp", region_name=REGION_NAME)
 
+client = boto3.client("cognito-idp")
 
 def initiate_auth(username, password):
     try:
