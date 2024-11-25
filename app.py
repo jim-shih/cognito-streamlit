71f0113885f2d1ced1089a3bef14d6c8ba192f7a
jimshih <jim732883@gmail.com>
2024-11-25 10:59:30 +0800

    feat: add sign-up feature

--- app.py
+++ app.py
@@ -24,12 +24,16 @@ REGION_NAME = os.getenv("USER_POOL_REGION_NAME")
 
 client = boto3.client("cognito-idp", region_name=REGION_NAME)
 
-def initiate_auth(username, password):
+
+def _initiate_auth(username, password):
     try:
         response = client.initiate_auth(
             ClientId=CLIENT_ID,
             AuthFlow="USER_PASSWORD_AUTH",
-            AuthParameters={"USERNAME": username, "PASSWORD": password},
+            AuthParameters={
+                "USERNAME": username,
+                "PASSWORD": password
+            },
         )
         return response
     except client.exceptions.NotAuthorizedException:
@@ -41,37 +45,133 @@ def initiate_auth(username, password):
     return None
 
 
+def _check_user_exists(username):
+    try:
+        response = client.admin_get_user(UserPoolId=USER_POOL_ID,
+                                         Username=username)
+        return response
+    except client.exceptions.UserNotFoundException:
+        st.error("User does not exist")
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+    return None
+
+
+def _delete_user(username):
+    try:
+        response = client.admin_delete_user(UserPoolId=USER_POOL_ID,
+                                            Username=username)
+        return response
+    except client.exceptions.UserNotFoundException:
+        st.error("User does not exist")
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+    return None
+
+
+def _sign_up(username, password):
+    try:
+        response = client.sign_up(
+            ClientId=CLIENT_ID,
+            Username=username,
+            Password=password,
+            UserAttributes=[{
+                "Name": "email",
+                "Value": username
+            }],
+        )
+        st.success(
+            "Sign-up successful. Please check your email for the verification code."
+        )
+        st.session_state["show_verification"] = True
+        st.session_state["username"] = username
+        return response
+    except client.exceptions.UsernameExistsException:
+        st.error("Username already exists")
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+
+
+def _confirm_sign_up(username, verification_code):
+
+    try:
+        response = client.confirm_sign_up(ClientId=CLIENT_ID,
+                                          Username=username,
+                                          ConfirmationCode=verification_code)
+        st.success("Account verification successful. You can now login.")
+        st.session_state["user_created"] = True
+        st.session_state["email_verified"] = True
+        return response
+    except client.exceptions.CodeMismatchException:
+        st.error("Incorrect verification code")
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+
+
+def _resent_verification_code(username):
+    try:
+        response = client.resend_confirmation_code(ClientId=CLIENT_ID,
+                                                   Username=username)
+
+        st.success("Verification code resent successfully")
+        st.session_state["user_created"] = True
+        return response
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+
+
+def user_login_page():
+    st.title("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+
+        response = _initiate_auth(username, password)
+        if response:
+            st.balloons()
+            st.success("Login successful")
+            st.session_state["access_token"] = response[
+                "AuthenticationResult"]["AccessToken"]
+            st.session_state["refresh_token"] = response[
+                "AuthenticationResult"]["RefreshToken"]
+            st.session_state["id_token"] = response["AuthenticationResult"][
+                "IdToken"]
+            st.session_state["username"] = username
+            st.session_state["authenticated"] = True
+
+
+def user_signup_page():
+    st.title("Sign Up")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    st.session_state["user_created"] = False
+    st.session_state["email_verified"] = False
+    if st.button("Sign Up"):
+        _sign_up(username, password)
+
+    if st.session_state.get("show_verification"):
+        verification_code = st.text_input("Verification Code")
+        if st.button("Verify"):
+            _confirm_sign_up(st.session_state["username"], verification_code)
+        if st.button("Resend Verification Code"):
+            _resent_verification_code(st.session_state["username"])
+
+    if not st.session_state.get("user_created"):
+        if _check_user_exists(username) and (
+                not st.session_state["email_verified"]):
+            _delete_user(username)
+
+
 def main():
-    st.title("AWS Cognito Authentication")
+    st.title("Cognito Authentication")
+    st.sidebar.title("Menu")
+    menu = ["Login", "Sign Up"]
+    choice = st.sidebar.selectbox("Select Option", menu)
 
-    if "authenticated" not in st.session_state:
-        st.session_state.authenticated = False
-
-    if not st.session_state.authenticated:
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        if st.button("Login"):
-            response = initiate_auth(username, password)
-            if response and "AuthenticationResult" in response:
-                st.session_state.authenticated = True
-                st.session_state.access_token = response["AuthenticationResult"][
-                    "AccessToken"
-                ]
-                st.session_state.id_token = response["AuthenticationResult"]["IdToken"]
-                st.session_state.refresh_token = response["AuthenticationResult"][
-                    "RefreshToken"
-                ]
-                st.rerun()
-    else:
-        st.write("You are logged in!")
-        if st.button("Logout"):
-            for key in ["authenticated", "access_token", "id_token", "refresh_token"]:
-                if key in st.session_state:
-                    del st.session_state[key]
-            st.rerun()
-
-        # Protected content
-        st.write("This is protected content only visible to authenticated users.")
+    if choice == "Login":
+        user_login_page()
+    elif choice == "Sign Up":
+        user_signup_page()
 
 
 if __name__ == "__main__":
